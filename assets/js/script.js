72acf549a52e8e1c8983aaa2fd5cc3c104f8eaf7
Giorgio Saffirio <saffirio.giorgio@gmail.com>
2020-05-05 00:21:57 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,7 +24,7 @@ $(document).ready(function () {
   });
 
   // Add smooth scrolling to all links
-  $("a.nav-link).on("click", function (event) {
+  $("a.nav-link").on("click", function (event) {
     // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
       // Prevent default anchor click behavior
